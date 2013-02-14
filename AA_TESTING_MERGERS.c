fb9a02965978d52397e1a174d07029fe03f900f1
James Matthews <jimim@hotmail.co.uk>
2013-02-14 13:28:54 +0000

    Lets try a simpler version

--- AA_TESTING_MERGERS.c
+++ AA_TESTING_MERGERS.c
@@ -1,8 +1,4 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
 #include "atomic.h"
 #include "python.h"
 
@@ -12,40 +8,12 @@ This is just a test C function to see how mergers work.
 
 Imagine we have 2 workers, JM and OtherPerson
 
-1- JM initially wrote code (copied from photon_ge.c more or less)
-2- JM made some random changes
-
 **********************************************************/
 
-double main(r, tstar, freqmin, freqmax, ioniz_or_final, f)
+double main()
 {
-  double lumstar;
-  double log_g;
-  double emit, emittance_bb (), emittance_continuum ();
-  int spectype;
-
-  log_g = geo.gstar = log10 (G * geo.mstar / (geo.rstar * geo.rstar *2000.0)); /* JM Edited this */
+  double lumstar, r, tstar;
   lumstar = 8 * PI * STEFAN_BOLTZMANN * r * r * tstar * tstar * tstar; /* JM Edited this */
-
-  if (ioniz_or_final == 1)
-    spectype = geo.star_spectype;	/* type for final spectrum */
-  else
-    spectype = geo.star_ion_spectype;	/*type for ionization calculation */
-
-  if (spectype >= 0)
-    {
-      /* JM Edited this */
-      emit = emittance_continuum (spectype, freqmin, freqmax, tstar, log_g) *100.0; /* JM Edited this */
-    }
-  else
-    {
-      emit = emittance_bb (freqmin, freqmax, tstar);
-    }
-
-  *f = emit;			/* Calculate the surface flux between freqmin and freqmax */
-  *f *= (4. * PI * r * r);
-
-  geo.lum_star = lumstar;
   return (lumstar);
 }
 
