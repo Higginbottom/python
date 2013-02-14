c6b50cca1667fe7c2c8bcf0a5304086683c9051b
James Matthews <jimim@hotmail.co.uk>
2013-02-14 13:32:42 +0000

    OtherPerson edited test merger file

--- AA_TESTING_MERGERS.c
+++ AA_TESTING_MERGERS.c
@@ -13,7 +13,9 @@ Imagine we have 2 workers, JM and OtherPerson
 double main()
 {
   double lumstar, r, tstar;
-  lumstar = 8 * PI * STEFAN_BOLTZMANN * r * r * tstar * tstar * tstar; 
+  tstar=40000.0
+  lumstar = 8 * PI * STEFAN_BOLTZMANN * r * r * tstar * tstar * tstar;
+  // Other Person edited this differently 
   return (lumstar);
 }
 
