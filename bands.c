adcb0a54ffbfdea308e9fdef2bb501f2de27206c
jimim22 <jamesmatthews@ip-038-200.vpn.soton.ac.uk>
2013-05-15 18:52:21 +0100

    edited bands.c to 60000A

--- bands.c
+++ bands.c
@@ -116,7 +116,7 @@ bands_init (imode, band)
   /* Imported from python.c */
 
   // 59 - Increased to 20,000 A so could go further into NIR 
-  freqmin = C / 12000e-8;	/*20000 A */
+  freqmin = C / 60000e-8;	/*JM changed to 60000A for macro atoms */
   tmax = TSTAR; 
   if (geo.twind > tmax)
     tmax = geo.twind;
