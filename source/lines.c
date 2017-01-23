b3ddb5462d853b8ea13f6f1cc1ba98e1a69c73de
Nicholas Higginbottom <nick_higginbottom@fastmail.fm>
2017-01-22 17:57:51 -0800

    removed gbar check

--- source/lines.c
+++ source/lines.c
@@ -274,10 +274,10 @@ q21 (line_ptr, t)
     {
       omega = upsilon (line_ptr->coll_index, u0);
       gbar = omega / ECS_CONSTANT / line_ptr->gl / line_ptr->f * line_ptr->freq;        //check the implied value of gbar
-      if (gbar < 0.01 || gbar > 10)     //if it is odd (i.e. not about 1) throw an error
-      {
-        Error ("q21 - suspicious implied value of gbar for coll strength record %i of %e\n", line_ptr->coll_index, gbar);
-      }
+//      if (gbar < 0.01 || gbar > 10)     //if it is odd (i.e. not about 1) throw an error
+//      {
+//        Error ("q21 - suspicious implied value of gbar for coll strength record %i of %e\n", line_ptr->coll_index, gbar);
+//      }
     }
 
 
