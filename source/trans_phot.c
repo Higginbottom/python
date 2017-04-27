a7ef3a1180803b38b952ca5bcd10ff813bc64c66
Nicholas Higginbottom <nick_higginbottom@fastmail.fm>
2017-04-27 07:35:39 +0100

    more output

--- source/trans_phot.c
+++ source/trans_phot.c
@@ -281,8 +281,7 @@ trans_phot_single (WindPtr w, PhotPtr p, int iextract)
 
   n = 0;                        // Needed to avoid 03 warning, but it is not clear that it is defined as expected.
   iflag=0;
-  printf ("origin_orig=%i origin=%i\n",pp.origin_orig,pp.origin);
-	if (pp.origin_orig==PTYPE_AGN)
+	if (pp.origin==PTYPE_AGN)
 		iflag=1;
 
   /* This is the beginning of the loop for each photon and executes until the photon leaves the wind */
